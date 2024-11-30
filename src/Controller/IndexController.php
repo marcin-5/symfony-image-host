53f2c84eecb23281ac73bb1ccd7fe7435964572b
Marcin Bojara <marcin_5@mail.ru>
2024-11-30 08:49:31 +0100

    Add file upload functionality with alerts support
    Integrated file upload feature in IndexController and added user feedback alerts for success and error states. Updated stylesheet with new alert classes for custom alert styling, and included the ext-intl library to support filename transliteration.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
-use App\Form\UplodaPhotoType;
+use App\Entity\Photo;
+use App\Form\UploadPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function __construct(private readonly \Doctrine\Persistence\ManagerRegistry $doctrine)
     {
-        $form = $this->createForm(UplodaPhotoType::class);
+    }
+
+    #[Route('/', name: 'app_index')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(UploadPhotoType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($this->getUser()) {
+                /** @var UploadedFile $pictureFileName */
+                $pictureFileName = $form->get('filename')->getData();
+                if ($pictureFileName) {
+                    $originalFilename = pathinfo($pictureFileName->getClientOriginalName(), PATHINFO_FILENAME);
+                    $safeFileName = transliterator_transliterate(
+                        'Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()',
+                        $originalFilename
+                    );
+                    try {
+                        $newFilename = $safeFileName . '-' . uniqid() . '.' . $pictureFileName->guessExtension();
+                        $pictureFileName->move('images/hosting', $newFilename);
+
+                        $entityPhoto = new Photo();
+                        $entityPhoto->setFilename($newFilename);
+                        $entityPhoto->setPublic($form->get('public')->getData());
+                        $entityPhoto->setUploadedAt(new \DateTimeImmutable());
+                        $entityPhoto->setUser($this->getUser());
+
+                        $entityManager = $this->doctrine->getManager();
+                        $entityManager->persist($entityPhoto);
+                        $entityManager->flush();
+
+                        $this->addFlash('success', 'Plik został wgrany pomyślnie.');
+                    } catch (\Exception $e) {
+                        $this->addFlash('error', 'Wystąpił nieoczekiwany błąd.<br>' . $e->getMessage());
+                    }
+                }
+            }
+        }
 
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
