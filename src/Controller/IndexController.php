4eb7fb8322ec96439705ad2bca07164b11da8038
Marcin Bojara <marcin_5@mail.ru>
2024-11-30 05:28:55 +0100

    Add photo upload form to index page
    Created a new form type `UplodaPhotoType` to handle photo uploads with options for file constraints and public visibility. Integrated this form into the index page template and updated the controller to pass the form to the view. Adjusted the front-end template to utilize Symfony's form rendering functions.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\UplodaPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,11 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        $form = $this->createForm(UplodaPhotoType::class);
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/UplodaPhotoType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Photo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class UplodaPhotoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('filename', FileType::class, [
+                'label' => 'Wybierz plik',
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/*',
+                        ],
+                        'mimeTypesMessage' => 'Można przesłać tylko pliki w formacie obrazu.',
+                    ])
+                ]
+            ])
+            ->add('is_public', CheckboxType::class, [
+                'label' => 'Publiczny',
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Photo::class,
+        ]);
+    }
+}
