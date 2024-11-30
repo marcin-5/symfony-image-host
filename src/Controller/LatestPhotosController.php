64bd635b8c1c3947b623c8810d56d86e791e0835
Marcin Bojara <marcin_5@mail.ru>
2024-11-30 16:04:57 +0100

    Add LatestPhotosController to display recent public photos
    Introduced a new controller that fetches and displays the latest public photos using the Doctrine ORM. The controller leverages Symfony's routing and templating to provide a page at the '/latest' route, rendering photos in descending order of upload date.

---
+++ src/Controller/LatestPhotosController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Photo;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LatestPhotosController extends AbstractController
+{
+    public function __construct(private readonly ManagerRegistry $doctrine)
+    {
+    }
+
+    #[Route('/latest', name: 'app_latest_photos')]
+    public function index(): Response
+    {
+        $entityManager = $this->doctrine->getManager();
+        $latestPhotosPublic = $entityManager->getRepository(Photo::class)->findBy(['is_public' => true],
+            ['uploaded_at' => 'DESC'],
+        );
+        return $this->render('latest_photos/index.html.twig', [
+            'latestPhotosPublic' => $latestPhotosPublic,
+        ]);
+    }
+}
